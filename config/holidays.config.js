2fef33b025a093c7147befe358871c46141f2aab
Darryl Pargeter <darrylpargeter@gmail.com>
2018-10-07 12:08:51 +0100

    basic commit

---
+++ config/holidays.config.js
@@ -0,0 +1,44 @@
+const holidays = [
+
+    {
+        'description' : 'New Years Day 2017',
+        'start'       : '01-01-2017',
+        'end'         : '01-01-2017'
+    },
+
+    {
+        'description' : 'Good Friday 2017',
+        'start'       : '14-04-2017',
+        'end'         : '14-04-2017'
+    },
+
+    {
+        'description' : 'Spring Bank Holiday 2017',
+        'start'       : '01-05-2017',
+        'end'         : '01-05-2017'
+    },
+
+    {
+        'description' : 'Christmas Day 2017',
+        'start'       : '25-12-2017',
+        'end'         : '25-12-2017'
+    },
+
+    {
+        'description' : 'Christmas Holidays 2017',
+        'start'       : '20-12-2017',
+        'end'         : '30-12-2017'
+    },
+
+    {
+        'description' : 'New Years Eve 2017',
+        'start'       : '31-12-2017',
+        'end'         : '31-12-2017'
+    },
+
+    {
+        'description' : 'New Years Day 2018',
+        'start'       : '01-01-2018',
+        'end'         : '01-01-2018'
+    }
+];
